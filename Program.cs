b1c26825cf86d1389a80d2c16f039d52591250e0
Mack <mack_ignacio@yahoo.com>
2019-12-13 03:08:54 +0800

    Add(files) - Project files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sms_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
